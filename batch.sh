433e0531862b49982577c93dedf16c6447a81b58
nathansharris <nathansharris@gmail.com>
2020-01-02 12:22:44 -0700

    Make functional.

--- batch.sh
+++ batch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for first in `seq 56000 50 56950`;
-    do sbatch para.slr first `bc <<< $first+49`;
+    do sbatch --export=first=$first,last=`bc <<< $first+49` para.slr;
     #do echo "$first" `bc <<< "$first"+49`;
 done

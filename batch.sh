7b3937413dd7eed6a2e038f5f6a3423778529df1
nathansharris <nathansharris@gmail.com>
2020-01-02 12:49:09 -0700

    Everything will run now besides dep.sh.

--- batch.sh
+++ batch.sh
@@ -2,6 +2,8 @@
 
 #One way to do this is to pass variables to slurm with --export. 
 for first in `seq 56000 50 56950`;
+    # If we iterate over the values, be sure that the job, out, and err names are 
+    # unique for each set. 
     do sbatch --export=first=$first,last=`bc <<< $first+49` -J "$first" -o "$first".out -e "$first".err para.slr;
     #do echo "$first" `bc <<< "$first"+49`;
 done

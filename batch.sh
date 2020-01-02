bc121d501d2c457c7ba4442022bcd44f3ff7cd13
nathansharris <nathansharris@gmail.com>
2020-01-02 12:39:04 -0700

    .

--- batch.sh
+++ batch.sh
@@ -2,6 +2,6 @@
 
 #One way to do this is to pass variables to slurm with --export. 
 for first in `seq 56000 50 56950`;
-    do sbatch --export=first=$first,last=`bc <<< $first+49` para.slr;
+    do sbatch --export=first=$first,last=`bc <<< $first+49` -J "$first" -o "$first".out -e "$first".err para.slr;
     #do echo "$first" `bc <<< "$first"+49`;
 done

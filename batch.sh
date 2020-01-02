e85793c9a628733a8bc7065599f06c3e8addbc36
nathansharris <nathansharris@gmail.com>
2020-01-02 12:27:05 -0700

    Making functional.

--- batch.sh
+++ batch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#One way to do this is to pass variables to slurm with --export. 
 for first in `seq 56000 50 56950`;
     do sbatch --export=first=$first,last=`bc <<< $first+49` para.slr;
     #do echo "$first" `bc <<< "$first"+49`;
